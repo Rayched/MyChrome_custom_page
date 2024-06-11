9c00ad64c5794d299b54f9109140640aa447d3e6
Rayched <ny2401@naver.com>
2024-06-11 16:42:01 +0900

    검색창: 구글 검색 결과 출력하는 기능 구현 완료

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,15 +3,23 @@ import { KeyboardEvent, useState } from "react";
 function SearchBar(){
     const [Keyword, setKeyword] = useState("");
 
-    const getKeywords = (event: any) => {
+    //Input 요소에 입력된 텍스트를 state에 저장
+    const getKeywords = (event: any) => {  
         setKeyword(event.target.value);
+    }
+
+    //검색어에 대한 구글 검색 결과를 요청하는 함수
+    const ShowResult = () => {
+        let SearchWord = Keyword;
+        window.location.href = `https://www.google.com/search?q=${SearchWord}&sourceid=chrome&ie=UTF-8`;
+        setKeyword("");
     }
 
     //Enter키를 누르면 특정 logic 실행
     const EnterKeys = (event: KeyboardEvent) => {
         if(event.code === "Enter"){
             console.log(Keyword);
-            setKeyword("");
+            ShowResult();
         }
     }
 
